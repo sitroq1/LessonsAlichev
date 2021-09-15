348cfda2f80a671a7e5671d1851eed0d1da5a365
sitroq1 <sitroq@gmail.com>
2021-09-15 14:50:00 +0300

    Добавил третьего человека и исправил ошибку

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,17 +1,18 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
-    Person person1 = new Person();
-    person1.name = "Max";
-    person1.age = 30;
+        Person person1 = new Person();
+        person1.name = "Max";
+        person1.age = 30;
 
-    Person person2 = new Person();
-    person2.name = "Alex";
-    person2.age = 33;
+
+        Person person2 = new Person();
+        person2.name = "Alex";
+        person2.age = 33;
+
+        Person person3 = new Person();
+        person3.name = "Dima";
+        person3.age = 35;
     }
-
-    Person person3 = new Person();
-    person3.name = "Dima";
-    person3.age = 35;
 }
 class Person {
     String name;
