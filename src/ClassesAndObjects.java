41f3698bc8f8737105e8d15d90ef7b847d0c8d0f
sitroq1 <sitroq@gmail.com>
2021-09-15 13:43:12 +0300

    Добавил первого человека

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,8 +1,11 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
-
+    Person person1 = new Person();
+    person1.name = "Max";
+    person1.age = 30;
     }
 }
 class Person {
-
+    String name;
+    int age;
 }
