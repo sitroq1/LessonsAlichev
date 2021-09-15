2a9dfd839b238483ab8abe0e0c3eaf0f2675a992
sitroq1 <sitroq@gmail.com>
2021-09-15 14:24:04 +0300

    Добавил третьего человека

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -8,6 +8,10 @@ public class ClassesAndObjects {
     person2.name = "Alex";
     person2.age = 33;
     }
+
+    Person person3 = new Person();
+    person3.name = "Dima";
+    person3.age = 35;
 }
 class Person {
     String name;
