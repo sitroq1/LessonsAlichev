b49458b24f58e583b2bea9370bca59e066ff21cf
sitroq1 <sitroq@gmail.com>
2021-09-16 23:56:24 +0300

    Добавил вывод

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,10 +3,12 @@ public class ClassesAndObjects {
     Person person1 = new Person();
     person1.name = "Max";
     person1.age = 30;
+    System.out.println("Вас зовут " + person1.name + ", Вам " + person1.age + " лет");
 
     Person person2 = new Person();
     person2.name = "Alex";
     person2.age = 33;
+    System.out.println("Вас зовут " + person1.name + ", Вам " + person1.age + " лет");
     }
 }
 class Person {
